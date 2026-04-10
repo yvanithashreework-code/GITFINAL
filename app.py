7b2bfceadfce581d62fb538ff731e70ead317a26
[Vanitha Shree] <[yvanithashreework@gmail.com]>
2026-04-11 00:22:02 +0530

    Modified by DeveloperA

---
+++ app.py
@@ -0,0 +1,10 @@
+class parent:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def intro(self):
+        return f"the child name is {self.name} and its age is {self.age}"
+        
+
+a=parent("Shree", 21)
+a.intro()        
