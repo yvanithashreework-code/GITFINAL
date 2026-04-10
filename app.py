fda4e64ef944a81983195be81963929119231ffe
[Vanitha Shree] <[yvanithashreework@gmail.com]>
2026-04-11 00:53:07 +0530

    Made changes again

--- app.py
+++ app.py
@@ -4,11 +4,9 @@ class parent:
         self.age=age
     def intro(self):
         return f"the child name is {self.name} and its age is {self.age}"
-        
-
 a=parent("Shree", 21)
 a.intro()    
 b=parent("Sneha",23)
 b.intro()
-c=parent("Sharma", 25)
-c.intro()
+
+
